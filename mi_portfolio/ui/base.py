4d47985e47a53340b334e6df4e9637f77029cbce
rperezpin <rubenpi1996@gmail.com>
2025-02-28 04:26:19 +0100

    Correccion fondo en iphone y navbar fijado

--- mi_portfolio/ui/base.py
+++ mi_portfolio/ui/base.py
@@ -5,7 +5,21 @@ from .navbar import navbar
 def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
     return rx.fragment(
         rx.box(id="top"),  # Punto de referencia para el scroll
-        navbar(),        
+        rx.box(
+            navbar(),
+            position="fixed",
+            top="0",
+            left="0",
+            right="0",
+            z_index="1000",
+            style={
+                "backdropFilter": "blur(10px)",
+                "backgroundColor": rx.color_mode_cond(
+                    light="rgba(255, 255, 255, 0.7)",  # Blanco semitransparente
+                    dark="rgba(0, 0, 0, 0.5)"  # Negro semitransparente
+                ),
+            }
+        ),
         rx.color_mode_cond(
             # En modo claro, aplicamos un fondo con imagen.
             light=rx.box(
@@ -13,11 +27,14 @@ def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
                 style={
                     "backgroundImage": "url('/bg2.jpg')",
                     "backgroundSize": "cover",
+                    "paddingTop": "80px",  # Espacio para la navbar fija
                     "backgroundPosition": "center",
                     "backgroundAttachment": "fixed",  # El fondo se queda fijo al hacer scroll
                     "width": "100vw",  # Ocupa el 100% del ancho del viewport
-                    "minHeight": "100vh",
-                    },
+                    "height": "100%",  # Nueva propiedad
+                    "minHeight": "-webkit-fill-available; min-height: 100vh",  # Fix para Safari
+                    "position": "relative",  # Mejor manejo de posición
+                },
             ),
             # En modo oscuro, solo mostramos el contenido sin imagen de fondo.
             dark=rx.box(
@@ -25,11 +42,14 @@ def base_page(child: rx.Component, *args, **kwargs) -> rx.Component:
                 style={
                     "backgroundImage": "url('/bg4.jpg')",
                     "backgroundSize": "cover",
+                    "paddingTop": "80px",  # Espacio para la navbar fija
                     "backgroundPosition": "center",
                     "backgroundAttachment": "fixed",  # El fondo se queda fijo al hacer scroll
                     "width": "100vw",  # Ocupa el 100% del ancho del viewport
-                    "minHeight": "100vh",
-                    },
+                    "height": "100%",  # Nueva propiedad
+                    "minHeight": "-webkit-fill-available; min-height: 100vh",  # Fix para Safari
+                    "position": "relative",  # Mejor manejo de posición
+                },
             ),
         ),
         rx.tablet_and_desktop(
